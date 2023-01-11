c065ed32c57240049a9ad8d36debd06fe5c59451
Dimitrov Ivan <mmo_unstable@abv.bg>
2023-01-11 11:54:27 +0200

    Refactor some implementations, mainly in the serverSideDB class, removing constructors and making the class more method oriented.

--- PMS_Server/src/Main/Server.java
+++ PMS_Server/src/Main/Server.java
@@ -4,14 +4,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Server implements Runnable {
 
 	public static int port = 1337;
 	public static ServerSocket serverSocket;
+	public static HashMap<String, Socket> onlineUsers;
 
 	Server() {
+		onlineUsers = new HashMap<String, Socket>();
 		try {
 			serverSocket = new ServerSocket(port);
 		} catch (IOException e) {
@@ -33,41 +38,52 @@ public class Server implements Runnable {
 			e.printStackTrace();
 		}
 
-		authentication(); // moje i client side no po-skoro taka shte ostane
+		authentication(link); // moje i client side no po-skoro taka shte ostane
 		syncClientWithServerDB();
 		handleClient(link);
-		
+
 	}
 
-	private void authentication() {
+	private void authentication(Socket link) {
 		// TODO Auto-generated method stub
-		while(true) {
-			//Въведи user И парола sus system in za test
-			//input = new Scanner(link.getInputStream()); 4akame ime i parola
+		while (true) {
+			// Въведи user И парола sus system in za test
+			// input = new Scanner(link.getInputStream()); 4akame ime i parola
 			Scanner input = new Scanner(System.in);
 			String username = input.nextLine();
 			String password = input.nextLine();
-			if(userExists(username)) {
-				if(correctLoginInfo(username,password)) {
-					// uspeshno sme se svurzali... 
+			ServerSideDB db = new ServerSideDB();
+			if (userExists(username, db)) {
+				if (correctLoginInfo(username, password, db)) {
+					onlineUsers.put(username, link);
 					break;
 				}
-			}else {
-				//create account
+			} else {
+				// create account
 			}
 		}
 	}
 
+	private boolean userExists(String username, ServerSideDB db) {
+		return db.isUser(username);
+	}
+
+	private boolean correctLoginInfo(String username, String password, ServerSideDB db) {
+		if (db.passwordIsCorrect(username, password)) {
+			// send client GUI notification to change view
+		}
+		return false;
+	}
 
 	private void handleClient(Socket link) {
-		
+
 		Scanner input = null;
 		try {
 			input = new Scanner(link.getInputStream());
 			String update = input.nextLine();
-			while(!(update = input.nextLine()).equals("-1")) {
+			while (!(update = input.nextLine()).equals("-1")) {
 				String[] data = update.split(",");
-				updateServerDB(data[0],data[1]);
+				updateServerDB(data[0], data[1]);
 			}
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
@@ -75,18 +91,17 @@ public class Server implements Runnable {
 		} finally {
 			input.close();
 		}
-		
+
 		try {
-			
+
 			input = new Scanner(link.getInputStream());
 			PrintWriter output = new PrintWriter(link.getOutputStream(), true);
 			int numMessages = 0;
-			
-			
+
 			String message = input.nextLine();
 			while (!message.equals("*CLOSE*")) {
-			handleMessage(message);
-			
+				handleMessage(message);
+
 				System.out.println("\nMessage received...");
 				numMessages++;
 				output.println("Message" + numMessages + ": " + message);
@@ -107,53 +122,69 @@ public class Server implements Runnable {
 		}
 
 	}
-	
 
 	private void updateServerDB(String updateType, String string2) {
 		// TODO Auto-generated method stub
-		if(updateType.equals("1")) {
-			//update client DATA in server db
+		if (updateType.equals("1")) {
+			// update client DATA in server db
 		} else {
-			//update add client messages in chatroom with name X 
+			// update add client messages in chatroom with name X
 		}
 	}
 
-	private boolean correctLoginInfo(String username, String password) {
-		// Пробвай да дръшнеш usera i parolata ot bazata za " username " ako sa ravni vurni true;
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	private boolean userExists(String username) {
-		// Провери в базата дали съществува и върни труе ако съществува, ако не връща 
-		return false;
-		// TODO Auto-generated method stub
-		
-	}
-
 	private void syncClientWithServerDB() {
-		// Select every entry in DB between client's last logout and current login time now() and send them to client vs array of messages or new DB with the entries between those 2 dates
+		// Select every entry in DB between client's last logout and current login time
+		// now() and send them to client vs array of messages or new DB with the entries
+		// between those 2 dates
 	}
 
 	private void handleMessage(String message) {
 		// TODO Auto-generated method stub
-		// Index: 0 - Chat_Room_ID; 1 - MessageType(text,image,text+image,voice); 2: Message
+		// Index: 0 userSending 1 - Chat_Room_ID; 2 -
+		// MessageType(text,image,text+image,voice); 3:
+		// Message
 		String[] msg = message.split(",");
 		// - Update DB char_ROOM
 		// ----->>>> Insert Code here
 		// Select members in room that changed state
 		String sql = "Select User_ID from char_room_wharehouse where char_room_ID = msg[0]";
-		new ServerSideDB(msg,TasksDB.notifyUsers);
-		
+		ServerSideDB db = new ServerSideDB();
+		db.setArgs(msg);
+		db.setTask(TasksDB.notifyUsers);
+		ArrayList<String> users = null;
+		try {
+			users = db.selectRoomUsers();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		for (String user : users) {
+			if (isOnline(user) && !user.equals(msg[0])) {
+				updateUser(db, user);
+			}
+		}
 		// notify room members if online to request update.
 		notifyOnline();
-		
+
 	}
-	
+
+	private void updateUser(ServerSideDB db, String user) {
+		// TODO Auto-generated method stub
+		// fetch user socket from map and send message to " USER "
+
+	}
+
+	private boolean isOnline(String user) {
+		if (onlineUsers.get(user) != null) {
+			return true;
+		}
+		return false;
+	}
 
 	private void notifyOnline() {
 		// prepovtarq se sus ServerSideDB notify
-		
+
 	}
 
 	public static void main(String[] args) {
--- PMS_Server/src/Main/ServerSideDB.java
+++ PMS_Server/src/Main/ServerSideDB.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class ServerSideDB {
 
@@ -14,63 +15,102 @@ public class ServerSideDB {
 	static final String DB_URL = "jdbc:mysql://localhost/girrafe";
 	static final String USER = "root";
 	static final String PASS = "dCBZXTf49PcL3L97lWXP";
+	Connection conn;
+	Statement stmt;
+	Enum task;
+	String[] args;
 
-	ServerSideDB(String[] args, Enum task) {
-		Connection conn = null;
-		Statement stmt = null;
+	ServerSideDB() {
+		conn = null;
+		stmt = null;
 		try {
 			Class.forName(JDBC_DRIVER);
 			System.out.println("Connecting to database...");
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			stmt = conn.createStatement();
-	
-			if(task.ordinal() == 1) {
-				ResultSet resultSet = selectRoomUsers(stmt,args);
-				while (resultSet.next()) {
-					notifyUser(resultSet.getString(1));
-				}
-			}
-			else if(task.ordinal() == 2) {
-				// some other code until we do all cases
-			}
-
-			
 
 		} catch (SQLException se) {
 			se.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (stmt != null) {
-					stmt.close();
-				}
-			} catch (SQLException se) {
-				se.printStackTrace();
-			}
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException se) {
-				se.printStackTrace();
-			}
-
 		}
 		System.out.println("Query has been executed and connection has been closed");
 	}
 
+	public void closeConnection() {
+
+		try {
+			if (stmt != null) {
+				stmt.close();
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+	}
+
+	private void doSomething() {
+
+		if (task.ordinal() == 2) {
+			// some other code until we do all cases
+		}
+
+	}
+
+	// returns resultSet with all user_ID's that equal the args[0]
+	public ArrayList<String> selectRoomUsers() throws SQLException {
+		// Maybe change to String[] after tests
+		ArrayList<String> users = new ArrayList<String>();
+		String sql = "Select User_ID from char_room_wharehouse where char_room_ID = args[1]";
+		stmt.executeQuery(sql);
+		if (task.ordinal() == 1) {
+			ResultSet resultSet = null;
+			try {
+				resultSet = stmt.getResultSet();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	
+			try {
+				while (resultSet.next()) {
+					try {
+						users.add(resultSet.getString(1));
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	
+		return users;
+	}
+
 	private void notifyUser(String user) {
-		if(isOnline(user)) {
+		if (isOnline(user)) {
+
 			// napravi neshto
 		}
 		// TODO Auto-generated method stub
-		// check if user is online and if true = send update 
-		if(isOnline(user)) {
-			
-		};
-		// else if offline do nothing, and when user gets online he will ask for updates when connecting(will send his db i guess and we compare to our and send him updated tables info which he can imput in his db)
+		// check if user is online and if true = send update
+		if (isOnline(user)) {
+
+		}
+		;
+		// else if offline do nothing, and when user gets online he will ask for updates
+		// when connecting(will send his db i guess and we compare to our and send him
+		// updated tables info which he can imput in his db)
 	}
 
 	private boolean isOnline(String user) {
@@ -79,14 +119,57 @@ public class ServerSideDB {
 		return false;
 	}
 
-	// returns resultSet with all user_ID's that equal the args[0]
-	public static ResultSet selectRoomUsers(Statement stmt, String[] args) throws SQLException {
-		String sql = "Select User_ID from char_room_wharehouse where char_room_ID = args[0]";
-		stmt.executeQuery(sql);
-		return stmt.getResultSet();
+	public Enum getTask() {
+		return task;
 	}
 
+	public void setTask(Enum task) {
+		this.task = task;
+	}
 
+	public String[] getArgs() {
+		return args;
+	}
+
+	public void setArgs(String[] args) {
+		this.args = args;
+	}
+
+	public boolean isUser(String username) {
+		String sql = "Select username from char_room_wharehouse where username = @username";
+		try {
+			stmt.executeQuery(sql);
+			ResultSet rs = stmt.getResultSet();
+			while(rs.next()) {
+				if(rs.getString(1) != null) {
+					return true;
+				}
+			}
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	public boolean passwordIsCorrect(String username, String password) {
+		// TODO Auto-generated method stub
+		String sql = "Select password from char_room_wharehouse where username = @username";
+		try {
+			stmt.executeQuery(sql);
+			ResultSet rs = stmt.getResultSet();
+			while(rs.next()) {
+				if(rs.getString(1).equals(password)) {
+					return true;
+				}
+			}
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
 
 }
-
