e32cdd5178228b4a54a247fa2fbb4857911d1de9
Dimitrov Ivan <mmo_unstable@abv.bg>
2023-01-13 17:16:11 +0200

    commiting new GUI class

---
+++ PMS_Server/src/Main/GUI.java
@@ -0,0 +1,84 @@
+package Main;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.TextArea;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ConcurrentModificationException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class GUI {
+
+	JFrame frame;
+	static JTextArea textArea;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void startGUI() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(2200, 100, 1000, 650);
+		frame.setLayout(new FlowLayout());
+		JPanel buttons = new JPanel();
+		JPanel outputArea = new JPanel();
+		JButton print = new JButton("PRINT USERS");
+		textArea = new JTextArea();
+		outputArea.setPreferredSize(new Dimension(750,500));
+		textArea.setPreferredSize(new Dimension(750,500));
+		outputArea.add(textArea);
+		buttons.add(print);
+		frame.add(outputArea);
+		frame.add(buttons);
+	
+		
+		print.addActionListener(e -> selectionButtonPressed());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private Object selectionButtonPressed() {
+		printArea();
+		return null;
+	}
+	static void printArea() {
+		StringBuffer concat = new StringBuffer();
+		Server.order = "Print";
+		new StringBuffer();
+		try {
+			Server.onlineUsers.forEach(
+					(key, value) -> concat.append("Active UserName: :" + key + "Active user password: " + value+"\n"));
+		} catch (ConcurrentModificationException e) {
+			System.err.println("Nishkite neshto ne se razbraha!");
+		}
+		textArea.setText(concat.toString());
+	}
+
+}
