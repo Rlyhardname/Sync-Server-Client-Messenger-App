dc52a31bfb34a5b402fa0cba9eb9b438ee855cca
Dimitrov Ivan <mmo_unstable@abv.bg>
2023-01-14 18:48:13 +0200

    Got client/Server communication to work, now just have to do cross client and at least chat works

--- PMS_Server/src/Main/ClientGUI.java
+++ PMS_Server/src/Main/ClientGUI.java
@@ -3,13 +3,18 @@ package Main;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.Color;
 
 
 //import com.mysql.cj.xdevapi.Client;
@@ -20,8 +25,10 @@ public class ClientGUI {
 	private static int PORT = 1337;
 	private JFrame frame;
 	private ClientLogic client;
-	private JTextField text;
+	private JTextArea tArea;
 	private JButton newClient;
+	private JScrollPane scrollPane;
+	String chatBox;
 
 	/**
 	 * Launch the application.
@@ -57,12 +64,22 @@ public class ClientGUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);		
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.getContentPane().setLayout(new FlowLayout());
-		text = new JTextField();
-		text.setPreferredSize(new Dimension(200,50));
+		//frame.getContentPane().setLayout(new FlowLayout());
+		frame.getContentPane().setLayout(new GridLayout());
+		
+		JPanel jp = new JPanel();
+		jp.setPreferredSize(new Dimension(500,500));
 		JButton sendText = new JButton("SEND TEXT");
-		frame.getContentPane().add(text);
+		tArea = new JTextArea(10,20);
+		tArea.setForeground(new Color(135, 206, 250));
+		tArea.setBackground(new Color(0, 0, 0));
+		tArea.setLineWrap(true);
+		//tArea.setPreferredSize(new Dimension(200,50));
+		scrollPane = new JScrollPane(tArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		jp.add(scrollPane);
+		frame.getContentPane().add(jp);
 		frame.getContentPane().add(sendText);
+		chatBox = "";
 		
 		newClient = new JButton("newClient");
 		frame.getContentPane().add(newClient);
@@ -77,17 +94,19 @@ public class ClientGUI {
 	}
 
 	private Object selectionButtonPressed(ClientLogic client) {
-		String msg = text.getText().toString();
+		String msg = tArea.getText().toString();
 		client.sendMessage(msg);
 		
 		String text1 = "";
 		try {
-			text1 = client.getInput().readLine();
+			text1 +=  client.getInput().readLine();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		text.setText(text1);
+		text1 = "THIS IS GOING TO BE A VERY LONG TEXT!";
+		tArea.append((text1+"\n"));
+
 		return null;
 	}
 
