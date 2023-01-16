24039cac15e0f05b96572a84a1747dbf46870a69
Dimitrov Ivan <mmo_unstable@abv.bg>
2023-01-16 16:23:58 +0200

    ServerSettings file and Jgoogles forms

---
+++ PMS_Server/src/Main/ServerSettings.java
@@ -0,0 +1,35 @@
+package Main;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ServerSettings {
+
+	public static final int PORT = 1337;
+	public static ServerSocket serverSocket;
+	public static ConcurrentHashMap<String, Socket> onlineUsers;
+	public static ServerGUI serverGUI;
+
+	ServerSettings(){
+		
+	}
+	
+	public static void serverDefaultSettings(ServerGUI gui) {
+
+		try {
+			serverGUI = gui;
+			serverSocket = new ServerSocket(PORT);
+			onlineUsers = new ConcurrentHashMap<String, Socket>();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
