c5972db8690ea44832442b86fb70723a65af7037
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-02-12 13:42:10 +0200

    -Refactor:    -Removed unnecessary components from object fields    -Remove useless window component from constructor    -add singleton pattern to constructor    -rework newConnection()    -refactor initialize() && add comments for more clarity    -removed useless printsLine() calls in printArea()

--- src/main/java/server/ServerGUI.java
+++ src/main/java/server/ServerGUI.java
@@ -4,169 +4,130 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.util.ConcurrentModificationException;
+import java.util.Objects;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
-import client.ClientLoginGUI;
+import client.LoginGUI;
 
 public class ServerGUI {
+    private ServerSettings settings;
+    private static ServerGUI serverGUI;
+    private JFrame frame;
+    static JTextArea textArea;
 
-	JFrame frame;
-	JButton newClientLogin;
-	static JTextArea textArea;
-	private static ServerGUI serverGUIstatic;
-	ServerSettings settings;
-	
+    /**
+     * Launch the application.
+     */
+    public static void startGUI() {
+        EventQueue.invokeLater(() -> {
+            try {
+                if (Objects.isNull(serverGUI)) {
+                    serverGUI = new ServerGUI();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-	public static ServerGUI getThisFrame() {
-		return serverGUIstatic;
-	}
+        });
 
-	public static void setThisFrame(ServerGUI window) {
-		ServerGUI.serverGUIstatic = window;
-	}
+    }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void startGUI() {
-		
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ServerGUI window = new ServerGUI();
-					window.frame.setVisible(true);
-					setThisFrame(window);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
-	}
-	
-	 void kill() {
-		frame.dispose();
-	}
-	
+    /**
+     * Create the application.
+     */
+    public ServerGUI() {
+        initialize();
+        settings = new ServerSettings();
+        ServerSettings.serverDefaultSettings(this);
+        while (true) {
+            ServerVer2 serverV2 = new ServerVer2();
+            if (Objects.nonNull(serverV2)) {
+                createNewConnection(serverV2);
+            } else {
+                // TODO log
+            }
+        }
 
+    }
 
-	/**
-	 * Create the application.
-	 */
-	public ServerGUI() {
-		initialize();
-		settings = new ServerSettings();
-		System.out.println(settings);
-		ServerVer2 serverV2 = new ServerVer2(this);
-		Thread serverV2Thread = new Thread(serverV2);
-		serverV2Thread.start();
-		createNewConnection();
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        // Init frame
+        frame = new JFrame();
+        frame.setBounds(200, 100, 1000, 650);
+        frame.setLayout(new FlowLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Init client button
+        JButton newClientLogin = new JButton("NEW CLIENT LOGIN");
+        // Init textArea for users information
+        textArea = new JTextArea();
+        textArea.setPreferredSize(new Dimension(750, 500));
+        // Init button pane and add buttons
+        JPanel buttons = new JPanel();
+        JButton print = new JButton("PRINT USERS");
+        buttons.add(print);
+        buttons.add(newClientLogin);
+        // Init printOut area and add widgets
+        JPanel outputArea = new JPanel();
+        outputArea.setPreferredSize(new Dimension(750, 500));
+        outputArea.add(textArea);
 
-	}
+        // Add button and printOutArea panes to frame
+        frame.add(outputArea);
+        frame.add(buttons);
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(200, 100, 1000, 650);
-		frame.setLayout(new FlowLayout());
-		JPanel buttons = new JPanel();
-		JPanel outputArea = new JPanel();
-		JButton print = new JButton("PRINT USERS");
-		newClientLogin = new JButton("NEW CLIENT LOGIN");
-		textArea = new JTextArea();
-		outputArea.setPreferredSize(new Dimension(750, 500));
-		textArea.setPreferredSize(new Dimension(750, 500));
-		outputArea.add(textArea);
-		buttons.add(print);
-		buttons.add(newClientLogin);
-		frame.add(outputArea);
-		frame.add(buttons);
+        new Thread(() -> print.addActionListener(e -> printUsersAndAdditionalInfo())).start();
+        new Thread(() -> newClientLogin.addActionListener(e -> clientLoginTestButton())).start();
+    }
 
-		Thread printThread = new Thread(new Runnable() {
+    private void createNewConnection(ServerVer2 serverVer2) {
+        new Thread(() -> {
+            serverVer2.connectClient();
+        }).start();
+    }
 
-			@Override
-			public void run() {
-				print.addActionListener(e -> selectionButtonPressed());
+    private Object printUsersAndAdditionalInfo() {
+        printArea();
+        return null;
+    }
 
-			}
+    private Object clientLoginTestButton() {
+        LoginGUI.startGUI();
+        ServerVer2 serverV2 = new ServerVer2();
+        createNewConnection(serverV2);
+        return null;
+    }
 
-		});
+    static void printArea() {
+        StringBuffer concat = new StringBuffer();
+        try {
+            if (!ServerSettings.onlineUsers.isEmpty()) {
+                ServerSettings.onlineUsers.forEach((key, value) -> concat
+                        .append("Active UserName: :" + key + "Active user password: " + value + "\n"));
+            }
 
-		Thread openNewClientWindow = new Thread(new Runnable() {
+        } catch (ConcurrentModificationException e) {
+            if (concat.equals("") || concat.length() == 0) {
+                textArea.setText("Бурканът преля... Обади се на техника!");
+            }
 
-			@Override
-			public void run() {
-				newClientLogin.addActionListener(e -> selectionButtonPressed1());
+        } catch (NullPointerException e1) {
+            if (concat.equals("") || concat.length() == 0) {
+                textArea.setText("Всички спят...");
+            }
 
-			}
+        }
 
-		});
+        if (!concat.equals("")) {
+            textArea.setText(concat.toString());
+        }
 
-		printThread.start();
-		openNewClientWindow.start();
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
-	public static void createNewConnection() {
-		Thread newClient = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				ServerVer2 newServerClient = new ServerVer2();
-				Thread serverTwoThread = new Thread(newServerClient);
-				serverTwoThread.start();
-				newServerClient.connectClient();
-			}
-
-		});
-		newClient.start();
-
-	}
-
-	private Object selectionButtonPressed() {
-		printArea();
-		return null;
-	}
-
-	private Object selectionButtonPressed1() {
-		ClientLoginGUI.startGUI();
-		createNewConnection();
-		return null;
-	}
-
-	static void printArea() {
-		StringBuffer concat = new StringBuffer();
-		try {
-
-			if (!ServerSettings.onlineUsers.isEmpty()) {
-				System.out.println("Printing Online Users");
-				ServerSettings.onlineUsers.forEach((key, value) -> concat
-						.append("Active UserName: :" + key + "Active user password: " + value + "\n"));
-
-			}
-
-		} catch (ConcurrentModificationException e) {
-			if (concat.equals("") || concat.length() == 0) {
-				textArea.setText("Бурканът преля... Обади се на техника!");
-			}
-			System.err.println("Всички спят...");
-		} catch (NullPointerException e1) {
-			if (concat.equals("") || concat.length() == 0) {
-				textArea.setText("Всички спят...");
-			}
-			System.err.println("Всички спят...");
-		}
-		if (!concat.equals("")) {
-			textArea.setText(concat.toString());
-		}
-
-	}
-
+    }
 
 }
