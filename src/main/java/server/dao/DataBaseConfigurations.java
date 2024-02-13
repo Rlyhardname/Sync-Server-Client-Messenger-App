2d8595701f89e138bbf8ba970bafe81805992e69
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-02-13 12:10:51 +0200

    -add Singleton pattern based DataSourcePool, for storing the current implementation of database manager used to store files. -add DataBaseConfigurations for holding the config attribute of our database - name,url,user,password -add MySqlDataSource factory method in DataBaseConfigurations for concrete MySQL data source.

---
+++ src/main/java/server/dao/DataBaseConfigurations.java
@@ -0,0 +1,64 @@
+package server.dao;
+
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+public class DataBaseConfigurations {
+    public class DataBaseConfiguration {
+        private String name;
+        private String url;
+        private String user;
+        private String password;
+
+        public DataBaseConfiguration(String name, String url, String user, String password) {
+            this.name = name;
+            this.url = url;
+            this.user = user;
+            this.password = password;
+        }
+
+        public MysqlDataSource newMysqlDataSource() {
+            MysqlConnectionPoolDataSource dataSource = new MysqlConnectionPoolDataSource();
+            dataSource.setDatabaseName(getName());
+            dataSource.setURL(getUrl());
+            dataSource.setUser(getUser());
+            dataSource.setPassword(getPassword());
+            return dataSource;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+
+
+    }
+}
---
+++ src/main/java/server/dao/DataSourcePool.java
@@ -0,0 +1,24 @@
+package server.dao;
+
+import javax.sql.DataSource;
+
+public class DataSourcePool {
+    private static DataSource dataSource;
+
+    public static DataSource instanceOf(DataSource dSource) {
+        if (dataSource == null) {
+            dataSource = dSource;
+        }
+        return dataSource;
+    }
+
+    public static DataSource instanceOf() {
+        return dataSource;
+    }
+
+    private static void resetSource() {
+        dataSource = null;
+    }
+
+
+}
