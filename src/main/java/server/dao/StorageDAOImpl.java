0b4992974a6ce9222668ec07c0b6853e903fdac8
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-02-22 16:20:16 +0200

    -Refactor and bugfix/optimize the new methods from for logging/saving messages and authorization.

--- src/main/java/server/dao/StorageDAOImpl.java
+++ src/main/java/server/dao/StorageDAOImpl.java
@@ -1,11 +1,12 @@
 package server.dao;
 
 import client.model.User;
+import common.Command;
 
 import javax.sql.DataSource;
-import java.io.FileInputStream;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Optional;
 
 public class StorageDAOImpl implements StorageDAO<User> {
@@ -22,13 +23,12 @@ public class StorageDAOImpl implements StorageDAO<User> {
     @Override
     public Optional<User> createUser(String username, String password) {
         String sql = "INSERT INTO User " + "VALUES(?,?)";
-        ResultSet rs = null;
         try (Connection conn = dataSource.getConnection();
              PreparedStatement prep = conn.prepareStatement(sql)) {
             prep.setString(1, username);
             prep.setString(2, password);
             prep.executeUpdate();
-            return Optional.of(new User(username,password));
+            return Optional.of(new User(username, password));
 
         } catch (SQLException e) {
             // TODO Auto-generated catch block
@@ -39,7 +39,7 @@ public class StorageDAOImpl implements StorageDAO<User> {
     }
 
     @Override
-    public void logUserActivity(String username) {
+    public void logUserLogout(String username) {
         String sql = "INSERT INTO user_log " + "(username,logout_time) " + "VALUES(?,NOW())";
         try (Connection conn = dataSource.getConnection();
              PreparedStatement prep = conn.prepareStatement(sql)) {
@@ -53,13 +53,26 @@ public class StorageDAOImpl implements StorageDAO<User> {
     }
 
     @Override
-    public void updateUserLog(String username) {
-        Timestamp dateTimeStamp = fetchTimestamp(username);
-        String sql = "UPDATE User_Log " + "SET allMessagesSent = ? " + "WHERE Logout_time = ? and username = ?";
-
+    public void logUserLogin(String username) {
+        String sql = "INSERT INTO user_log " + "(username,login_time) " + "VALUES(?,NOW())";
         try (Connection conn = dataSource.getConnection();
              PreparedStatement prep = conn.prepareStatement(sql)) {
-            prep.setInt(1, 0);
+            prep.setString(1, username);
+            prep.executeLargeUpdate();
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateUserLogMessageSent(String username,int state) {
+        Timestamp dateTimeStamp = fetchLastLogoutTimestamp(username);
+        String sql = "UPDATE User_Log " + "SET allMessagesSent = ? " + "WHERE Logout_time = ? and username = ?";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement prep = conn.prepareStatement(sql)) {
+            prep.setInt(1, state);
             prep.setTimestamp(2, dateTimeStamp);
             prep.setString(3, username);
             prep.executeLargeUpdate();
@@ -70,21 +83,23 @@ public class StorageDAOImpl implements StorageDAO<User> {
     }
 
     @Override
-    public Timestamp fetchTimestamp(String username) {
-        String sql = "SELECT Logout_time " + "FROM User_Log " + "WHERE username = ?";
+    public Timestamp fetchLastLogoutTimestamp(String username) {
+        String sql = "SELECT Logout_time " + "FROM User_Log " + "WHERE username = ? " + "ORDER BY Logout_time DESC LIMIT 1";
         Timestamp dateTimeStamp = null;
         try (Connection conn = dataSource.getConnection();
              PreparedStatement prep = conn.prepareStatement(sql)) {
             prep.setString(1, username);
             ResultSet rs = prep.executeQuery();
+
             while (rs.next()) {
-
-                Timestamp timestamp = rs.getTimestamp(1);
-
-                if (timestamp != null) {
-                    dateTimeStamp = timestamp;
+                dateTimeStamp = rs.getTimestamp(1);
+                if (Objects.nonNull(dateTimeStamp)) {
+                    System.err.println("Current Timestamp: " + dateTimeStamp.toString());
+                    //  return dateTimeStamp;
                 }
+
             }
+
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -102,74 +117,87 @@ public class StorageDAOImpl implements StorageDAO<User> {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+
     }
 
     @Override
     public String[] fetchAllByNameUnsentMessages(String username) {
-        // TODO Auto-generated method stub
-
         ResultSet rs = null;
-        Timestamp timestamp = fetchTimestamp(username);
-        boolean isUnsend = false;
-
+        Timestamp timestamp = fetchLastLogoutTimestamp(username);
         String sql = "SELECT allMessagesSent FROM User_log " + "WHERE logout_time = ? and allMessagesSent = ? ";
-
+        Timestamp ts;
         try (Connection conn = dataSource.getConnection();
              PreparedStatement prep = conn.prepareStatement(sql)) {
             prep.setTimestamp(1, timestamp);
             prep.setInt(2, 0);
             rs = prep.executeQuery();
             while (rs.next()) {
-                if (rs.getInt(1) == 0) {
-                    isUnsend = true;
+                int val = rs.getInt(1);
+                System.out.println("val : " + val);
+                if (val == 0) {
+                    return getBatch(username, timestamp);
                 }
-            }
 
-            if (isUnsend) {
-                String sql2 = "SELECT message_text,username,chat_room_id " + "FROM message_data "
-                        + "WHERE username != ? and timeLOG > ?";
-                ArrayList<String> messages = new ArrayList<String>();
-                try {
-                    prep.addBatch(sql2);
-                    prep.setString(1, username);
-                    prep.setTimestamp(2, timestamp);
-                    rs = prep.executeQuery();
-                    while (rs.next()) {
-                        String message = rs.getString(1);
-                        String user = rs.getString(2);
-                        int room = rs.getInt(3);
-                        messages.add(message + "," + user + "," + room + "," + "TextMessage");
-                    }
-
-                } catch (SQLException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-                String[] batch = messages.toArray(new String[0]);
-                return batch;
             }
 
         } catch (SQLException e1) {
-            // TODO Auto-generated catch block
             e1.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
         return new String[0];
+    }
 
+    private String[] getBatch(String username, Timestamp timestamp) {
+        String sql = "SELECT message_text,username,chat_room_id " + "FROM message_data "
+                + "WHERE username != ? and timeLOG > ?";
+        ArrayList<String> messages = new ArrayList<String>();
+        ResultSet rs = null;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement prep = conn.prepareStatement(sql)) {
+            prep.addBatch(sql);
+            prep.setString(1, username);
+            prep.setTimestamp(2, timestamp);
+            rs = prep.executeQuery();
+            while (rs.next()) {
+                String message = rs.getString(1);
+                String user = rs.getString(2);
+                int room = rs.getInt(3);
+                messages.add(Command.TEXT_MESSAGE.name() + "," + user + "," + room + "," + message);
+            }
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String[] batch = messages.toArray(String[]::new);
+        return batch;
     }
 
     @Override
-    public boolean storeMessage(String username, String msg, int room) {
+    public boolean isUserAuthorizedInRoom(String username, String msg, int room) {
         String sql = "SELECT username " + "FROM chat_room_warehouse " + "WHERE chat_room_id=? AND username=?";
-        String inputUser = "";
         try (Connection conn = dataSource.getConnection();
              PreparedStatement prep = conn.prepareStatement(sql)) {
             prep.setInt(1, room);
             prep.setString(2, username);
             ResultSet rs = prep.executeQuery();
-
             while (rs.next()) {
-                inputUser = rs.getString(1);
+                if (Objects.nonNull(rs.getString(1))) {
+                    return true;
+                }
 
             }
 
@@ -177,28 +205,30 @@ public class StorageDAOImpl implements StorageDAO<User> {
             // TODO Auto-generated catch block
             e1.printStackTrace();
         }
+        return false;
+    }
 
+    @Override
+    public void saveMessage(String username, String msg, int room) {
         String sql2 = "INSERT INTO Message_data " + "(chat_room_id,username,message_text) " + "VALUES(?,?,?)";
         try (Connection conn = dataSource.getConnection();
              PreparedStatement prep = conn.prepareStatement(sql2)) {
-            if (inputUser.toLowerCase().equals(username)) {
-                try {
-                    prep.setInt(1, room);
-                    prep.setString(2, inputUser);
-                    prep.setString(3, msg);
-                    prep.executeUpdate();
-
-                } catch (SQLException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-
+            try {
+                prep.setInt(1, room);
+                prep.setString(2, username);
+                prep.setString(3, msg);
+                prep.executeUpdate();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
+
+        } catch (
+                SQLException e) {
             throw new RuntimeException(e);
         }
-        return true;
     }
+
 
     public String[] getRoomUsers(int i) {
         String sql = "SELECT Username " + "FROM chat_room_warehouse " + "WHERE chat_room_id=?";
