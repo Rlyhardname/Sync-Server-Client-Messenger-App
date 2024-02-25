317377f1c83bcac169a1a530382edba51f731e73
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-02-25 21:33:40 +0200

    -add Push friendsList method for pushing friend list to clients every x seconds/minutes

--- src/main/java/server/ServerSettings.java
+++ src/main/java/server/ServerSettings.java
@@ -1,8 +1,10 @@
 package server;
 
+import common.Command;
+
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ServerSettings {
@@ -20,4 +22,40 @@ public class ServerSettings {
     }
 
     public static ConcurrentHashMap<String, ServerVer2> onlineUsers = new ConcurrentHashMap<>();
+
+//            Character.toString(0x00002705), //check mark
+//            Character.toString(0x1F60D), //cool face
+//            Character.toString(0x0001F5D9),// x symbol
+
+    public void pushFriendsList() {
+        String one = "1 ROOM                    ";
+        String two = "2 ROOM                    ";
+        String three = "3 ROOM                    ";
+        Random rand = new Random();
+        String[] testArr;
+        while (true) {
+            int order = rand.nextInt(3);
+            if (order == 0) {
+                testArr = new String[]{one, two, three};
+            } else if (order == 1) {
+                testArr = new String[]{two, three, one};
+            } else {
+                testArr = new String[]{three, one, two};
+            }
+
+            var friends = String.join(",", testArr);
+            for (var user : onlineUsers.entrySet()) {
+                user.getValue().getTextOutput().println(Command.PUSH_FRIENDS + "," + friends);
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+
+        }
+
+    }
+
 }
