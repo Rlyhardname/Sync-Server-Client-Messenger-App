a9face368822e65bc4c368e2dd878cc05f90e439
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-02-29 13:58:46 +0200

    -separate appending friends message into own method appendFriends -add pullFriends method for pushing friendList to specific client

--- src/main/java/server/util/Util.java
+++ src/main/java/server/util/Util.java
@@ -13,29 +13,40 @@ public class Util {
     public static void pushFriendsList() {
         StorageDAO DAO = new StorageDAOImpl(DataSourcePool.instanceOf());
         while (true) {
-            StringBuffer sb = new StringBuffer();
             for (var user : onlineUsers.entrySet()) {
                 Map<String, String> friends = DAO.getFriends(user.getKey());
-                for (var friend : friends.entrySet()) {
-                    if (sb.isEmpty()) {
-                        sb.append(Command.PUSH_FRIENDS.name());
-                    }
-                    sb.append(",");
-                    sb.append(friend.getValue());
-                    sb.append("   ");
-                    sb.append(friend.getKey());
-                }
-                user.getValue().getTextOutput().println(sb.toString());
-                sb = new StringBuffer();
+                user.getValue().getTextOutput().println(appendFriends(friends));
             }
+
             try {
-                Thread.sleep(5000);
+                Thread.sleep(60000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
 
         }
 
+    }
+
+    public static void pullFriends(String username) {
+        var printWriter = onlineUsers.get(username).getTextOutput();
+        StorageDAO DAO = new StorageDAOImpl(DataSourcePool.instanceOf());
+        Map<String, String> friendsAndStatus = DAO.getFriends(username);
+        printWriter.println(appendFriends(friendsAndStatus));
+    }
+
+    private static String appendFriends(Map<String, String> friends) {
+        StringBuffer sb = new StringBuffer();
+        for (var friend : friends.entrySet()) {
+            if (sb.isEmpty()) {
+                sb.append(Command.PUSH_FRIENDS.name());
+            }
+            sb.append(",");
+            sb.append(friend.getValue());
+            sb.append("   ");
+            sb.append(friend.getKey());
+        }
+        return sb.toString();
     }
 
 }
