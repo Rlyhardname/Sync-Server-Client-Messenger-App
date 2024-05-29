04dcc1e29e0edbd3d17a80367a1a782f2332358b
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-05-29 16:17:15 +0300

    -refactor misplaced field

--- src/main/java/server/ServerSettings.java
+++ src/main/java/server/ServerSettings.java
@@ -1,14 +1,12 @@
 package server;
 
-import common.Command;
-
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ServerSettings {
     public static final int PORT = 1337;
+    public static ConcurrentHashMap<String, ServerVer2> onlineUsers = new ConcurrentHashMap<>();
     public static ServerSocket serverSocket;
 
     static {
@@ -20,11 +18,5 @@ public class ServerSettings {
             throw new RuntimeException(e);
         }
     }
-
-    public static ConcurrentHashMap<String, ServerVer2> onlineUsers = new ConcurrentHashMap<>();
-
-
-
-
 
 }
