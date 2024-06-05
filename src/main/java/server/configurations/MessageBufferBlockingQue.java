8878960f39ed33876f747f20c1a5ad34b3cbd6fc
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-05 08:49:19 +0300

    -add experimental class MessageBufferBlockingQue for buffering incoming messages and displaying for later when needed(no concrete idea as of yet).

---
+++ src/main/java/server/configurations/MessageBufferBlockingQue.java
@@ -0,0 +1,20 @@
+package server.configurations;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MessageBufferBlockingQue {
+    private BlockingQueue<String> MessageBuffer;
+
+    private MessageBufferBlockingQue() {
+        MessageBuffer = new LinkedBlockingQueue<>();
+    }
+
+    public static MessageBufferBlockingQue instanceOf() {
+        return new MessageBufferBlockingQue();
+    }
+
+    public BlockingQueue<String> getMessageBuffer() {
+        return MessageBuffer;
+    }
+}
