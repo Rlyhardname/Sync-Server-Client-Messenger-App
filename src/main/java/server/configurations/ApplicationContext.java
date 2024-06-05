52f3b1b31fdbf21fd60b1825f7f5758858cc0e6d
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-05 08:50:41 +0300

    -Add ApplicationContext Singleton enum object/api handling user and message buffer operations.

---
+++ src/main/java/server/configurations/ApplicationContext.java
@@ -0,0 +1,62 @@
+package server.configurations;
+
+import server.services.ServerVer2;
+
+public enum ApplicationContext {
+    APPLICATION_CONTEXT;
+    private OnlineUsersMap onlineUsersMap;
+    private MessageBufferBlockingQue messageBufferBlockingQue;
+
+    public OnlineUsersMap getOnlineUsersHashMap() {
+        return onlineUsersMap;
+    }
+
+    public void setOnlineUsersHashMap(OnlineUsersMap onlineUsersMap) {
+        if (onlineUsersMap == null) {
+            this.onlineUsersMap = onlineUsersMap;
+        }
+    }
+
+    public void setMessageBufferBlockingQue(MessageBufferBlockingQue messageBufferBlockingQue) {
+        if (messageBufferBlockingQue == null) {
+            this.messageBufferBlockingQue = messageBufferBlockingQue;
+        }
+    }
+
+    public void initContext() {
+        messageBufferBlockingQue = MessageBufferBlockingQue.instanceOf();
+        onlineUsersMap = OnlineUsersMap.instanceOf();
+    }
+
+    public boolean isUserCountPositive() {
+        return onlineUsersMap.getOnlineUsers().size() > 0;
+    }
+
+    public boolean isUserOnline(String username) {
+        return onlineUsersMap.getOnlineUsers().containsKey(username);
+    }
+
+    public String concatUsersWithPasswordReturnString() {
+        StringBuffer concat = new StringBuffer();
+        onlineUsersMap.getOnlineUsers().forEach((key, value) -> concat
+                .append("Active UserName: :" + key + "Active user password: " + value + "\n"));
+
+        return concat.toString();
+    }
+
+    public void addUserAndServerInstance(String username, ServerVer2 serverVer2) {
+        onlineUsersMap.getOnlineUsers().put(username, serverVer2);
+    }
+
+    public void disconnectUser(String username) {
+        onlineUsersMap.getOnlineUsers().remove(username);
+    }
+
+    public ServerVer2 fetchServerInstance(String username) {
+        return onlineUsersMap.getOnlineUsers().get(username);
+    }
+
+    public void addAuthenticationAttempt(String message){
+        messageBufferBlockingQue.getMessageBuffer().add(message);
+    }
+}
