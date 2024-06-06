18ae6983ad569f8c8d948a98d14d1cab95a138b8
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-06 14:21:03 +0300

    -add enum Singleton Object Class Executor with:  -defaultImplementation of fixedThreadPool  -DE method InjectExecutor for executor custom implementation   -Execute() to handle runnable objects/events/method calls and phase out anonymous threads in the app.

---
+++ src/main/java/server/concurency/Executor.java
@@ -0,0 +1,26 @@
+package server.concurency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public enum Executor {
+    EXECUTOR;
+    private ExecutorService executorService;
+
+    public void injectExecutor(ExecutorService service) {
+        if (executorService == null) {
+            executorService = service;
+        }
+    }
+
+    public void defaultImplementation(int numberOfThreads) {
+        if (executorService == null) {
+            executorService = Executors.newFixedThreadPool(numberOfThreads);
+        }
+    }
+
+    public void execute(Runnable runnable) {
+        executorService.execute(runnable);
+    }
+
+}
